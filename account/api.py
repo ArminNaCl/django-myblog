14f147abb2de5902f54b53f08317f94092734b1f
ArminNaCl <armin.khoshnamak@gmail.com>
2021-01-23 22:35:35 +0330

    remove all apps

--- account/api.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.viewsets import ModelViewSet
-from .serializers  import UserSerializer
-from django.contrib.auth import get_user_model
-User =get_user_model()
-
-
-class UserViewSet(ModelViewSet):
-    serializer_class = UserSerializer
-    queryset = User.objects.all()
--- blog/urls.py
+++ blog/urls.py
@@ -16,37 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.conf import settings
 from django.urls import path , include
-from django.conf.urls.static import static
-from rest_framework.routers import DefaultRouter
-from account.api import UserViewSet
-
-router = DefaultRouter()
-router.register('user',UserViewSet)
-
-
-from account.views import (
-    RegisteritionView,
-    LoginView,
-    LogoutView
-)
-
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myblog.urls')),
-    path('api-auth/',include('rest_framework.urls')),
-    path('api/',include('myblog.APIUrls')),
-    path('api/',include(router.urls)),
-    path('register/', RegisteritionView.as_view(), name='register-url' ),
-    path('login/' , LoginView.as_view(), name = 'login-url'),
-    path('logout/', LogoutView.as_view(), name= 'logout-url'),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL,
      document_root=settings.MEDIA_ROOT)
